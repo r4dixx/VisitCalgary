794ced3da3477f309dc4cbc73b8a5a4d8272a652
r4dixx <amael.sikel@gmail.com>
2018-06-29 00:58:57 +0200

    ViewPager, TabLayout and CategoryAdapter implementations

--- app/src/main/java/com/r4dixx/visitcalgary/MainActivity.java
+++ app/src/main/java/com/r4dixx/visitcalgary/MainActivity.java
@@ -1,7 +1,9 @@
 package com.r4dixx.visitcalgary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewpager);
+
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }

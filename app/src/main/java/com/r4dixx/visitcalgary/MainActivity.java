17579e89d514e2bc62dbebcf4da2937557d372ae
snufflesrea <strait_viola@yahoo.com>
2018-07-03 17:54:34 +0800

    implement ButterKnife to MainActivity

--- app/src/main/java/com/r4dixx/visitcalgary/MainActivity.java
+++ app/src/main/java/com/r4dixx/visitcalgary/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.viewpager) ViewPager viewPager;
+    @BindView(R.id.sliding_tabs) TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,12 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.viewpager);
-
         CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
-
-        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 }

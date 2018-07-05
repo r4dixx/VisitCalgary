9accaa20359cbf32136e3ad3cdedd21e15883bbe
snufflesrea <strait_viola@yahoo.com>
2018-07-05 21:55:18 +0800

    implement ButterKnife library for ArtsFragment module

--- app/src/main/java/com/r4dixx/visitcalgary/ArtsFragment.java
+++ app/src/main/java/com/r4dixx/visitcalgary/ArtsFragment.java
@@ -1,6 +1,7 @@
 package com.r4dixx.visitcalgary;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,8 +10,41 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindDrawable;
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ArtsFragment extends Fragment {
+    @BindString(R.string.card_title_art_Ballet) String balletTitle;
+    @BindString(R.string.card_desc_art_Ballet)  String balletDesc;
+    @BindString(R.string.card_title_art_College) String collegeTitle;
+    @BindString(R.string.card_desc_art_College) String collegeDesc;
+    @BindString(R.string.card_title_art_Gallery) String galleryTitle;
+    @BindString(R.string.card_desc_art_Gallery) String galleryDesc;
+    @BindString(R.string.card_title_art_Dancesport) String danceTitle;
+    @BindString(R.string.card_desc_art_Dancesport)  String danceDesc;
+    @BindString(R.string.card_title_art_Theatre) String theaterTitle;
+    @BindString(R.string.card_desc_art_Theatre) String theaterDesc;
+    @BindString(R.string.card_title_art_Commons) String commonsTitle;
+    @BindString(R.string.card_desc_art_Commons) String commonsDesc;
+    @BindString(R.string.card_title_art_Amphitheatre) String amphitheaterTitle;
+    @BindString(R.string.card_desc_art_Amphitheatre) String amphitheaterDesc;
+    @BindString(R.string.card_title_art_Church) String churchTitle;
+    @BindString(R.string.card_desc_art_Church) String churchDesc;
+
+    @BindDrawable(R.drawable.art_ballet) int balletDraw;
+    @BindDrawable(R.drawable.art_college) int collegeDraw;
+    @BindDrawable(R.drawable.art_dancesport) int danceDraw;
+    @BindDrawable(R.drawable.art_amphitheatre) int amphiDraw;
+    @BindDrawable(R.drawable.art_church) int churchDraw;
+    @BindDrawable(R.drawable.art_commons) int commonDraw;
+    @BindDrawable(R.drawable.art_gallery) int galleryDraw;
+    @BindDrawable(R.drawable.art_theatre) int theaterDraw;
+
+    @BindView(R.id.places_list) ListView listView;
 
     public ArtsFragment() {
         // Required empty public constructor
@@ -20,20 +54,19 @@ public class ArtsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.places_list, container, false);
+        ButterKnife.bind(this,rootView);
 
         final ArrayList<Place> places = new ArrayList<>();
-        places.add(new Place(getString(R.string.card_title_art_Ballet), getString(R.string.card_desc_art_Ballet), R.drawable.art_ballet));
-        places.add(new Place(getString(R.string.card_title_art_College), getString(R.string.card_desc_art_College), R.drawable.art_college));
-        places.add(new Place(getString(R.string.card_title_art_Gallery), getString(R.string.card_desc_art_Gallery), R.drawable.art_gallery));
-        places.add(new Place(getString(R.string.card_title_art_Dancesport), getString(R.string.card_desc_art_Dancesport), R.drawable.art_dancesport));
-        places.add(new Place(getString(R.string.card_title_art_Theatre), getString(R.string.card_desc_art_Theatre), R.drawable.art_theatre));
-        places.add(new Place(getString(R.string.card_title_art_Commons), getString(R.string.card_desc_art_Commons), R.drawable.art_commons));
-        places.add(new Place(getString(R.string.card_title_art_Church), getString(R.string.card_desc_art_Church), R.drawable.art_church));
-        places.add(new Place(getString(R.string.card_title_art_Amphitheatre), getString(R.string.card_desc_art_Amphitheatre), R.drawable.art_amphitheatre));
+        places.add(new Place(balletTitle, balletDesc, balletDraw));
+        places.add(new Place(collegeTitle, collegeDesc, collegeDraw));
+        places.add(new Place(galleryTitle, galleryDesc, galleryDraw));
+        places.add(new Place(danceTitle, danceDesc, danceDraw));
+        places.add(new Place(theaterTitle, theaterDesc, theaterDraw));
+        places.add(new Place(commonsTitle, commonsDesc, commonDraw));
+        places.add(new Place(churchTitle, churchDesc, churchDraw));
+        places.add(new Place(amphitheaterTitle, amphitheaterDesc, amphiDraw));
 
         PlaceAdapter adapter = new PlaceAdapter(getActivity(), places);
-
-        ListView listView = rootView.findViewById(R.id.places_list);
         listView.setAdapter(adapter);
 
         return rootView;
